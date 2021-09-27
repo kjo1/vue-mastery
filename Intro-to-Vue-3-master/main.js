5378cff3e9d5947ada7ba4f66ca50f5a9b6e746f
Kanghyun Jo <kjo0@outlook.com>
2021-09-26 21:04:00 -0400

    L5-end: List Rendering (with own code challenge solution)

--- Intro-to-Vue-3-master/main.js
+++ Intro-to-Vue-3-master/main.js
@@ -3,8 +3,13 @@ const app = Vue.createApp({
 		return {
 			product: 'Socks',
 			image: './assets/images/socks_blue.jpg',
-			inventory: 7,
-			onSale: true,
+			isStock: true,
+			details: ['50% cotton', '30% wool', '20% polyester'],
+			variants: [
+				{ id: 2234, color: 'green' },
+				{ id: 2235, color: 'blue' },
+			],
+			sizes: ['Small', 'Medium', 'Large'],
 		};
 	},
 });
